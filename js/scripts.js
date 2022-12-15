189b3ca5cca3d8e50eec372f113db73adf37357e
Danyal Imran <dan.imran97@gmail.com>
2022-12-15 17:17:59 -0500

    finished project

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,6 @@ submitBtn.addEventListener("click", () => {
 
 rates.forEach((rate) => {
    rate.addEventListener("click", () => {
-      console.log("rate.innerHTML")
+      rating.innerHTML = rate.innerHTML
    }) 
 })
